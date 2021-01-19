172cedadcbef24312a07bfd6475472bdf8e046d8
Samir Pomak <eurekaa@abv.bg>
2021-01-19 17:16:33 +0200

    Added CSS to coloring style options

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,13 @@
 let wrap = document.querySelector('.wrap');
 let reset = document.querySelector('.reset');
+let resize = document.querySelector('.resize');
+let defaultSize = 16;
 
-window.addEventListener('load', setGrid(16, colorBlack));
+window.addEventListener('load', setGrid(defaultSize, colorBlack));
 reset.addEventListener('click', clearGrid);
+resize.addEventListener('click', resizeGrid);
 
-function setGrid(size = 16, colorStyle) {
+function setGrid(size = defaultSize, colorStyle) {
 	console.log(wrap);
 	wrap.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
 	for (let i = 0; i < size * size; i++) {
@@ -15,17 +18,38 @@ function setGrid(size = 16, colorStyle) {
 	}
 }
 
+function resizeGrid(size) {
+	removeGrid();
+	size = parseInt(prompt('Enter new size X for grid X by X:'));
+	if (size < 1 || size > 100) {
+		alert('Please enter a size between 1 - 100!');
+		resizeGrid();
+		return;
+	}
+	defaultSize = size;
+	if (selector.value == 'white') {
+		removeGrid();
+		setGrid(defaultSize, colorWhite);
+	} else if (selector.value == 'black') {
+		removeGrid();
+		setGrid(defaultSize, colorBlack);
+	} else if ((selector.value = 'RGB')) {
+		removeGrid();
+		setGrid(defaultSize, colorRGB);
+	}
+}
+
 let selector = document.querySelector('.coloring');
 selector.addEventListener('change', function () {
 	if (this.value == 'white') {
 		removeGrid();
-		setGrid(16, colorWhite);
+		setGrid(defaultSize, colorWhite);
 	} else if (this.value == 'black') {
 		removeGrid();
-		setGrid(16, colorBlack);
+		setGrid(defaultSize, colorBlack);
 	} else if ((this.value = 'RGB')) {
 		removeGrid();
-		setGrid(16, colorRGB);
+		setGrid(defaultSize, colorRGB);
 	}
 });
 
